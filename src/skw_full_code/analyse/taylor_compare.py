62186001c8d659a8d428fd795ee0569947566c02
James Tocknell <aragilar@gmail.com>
2018-09-13 13:21:53 +1000

    Fix label

--- src/skw_full_code/analyse/taylor_compare.py
+++ src/skw_full_code/analyse/taylor_compare.py
@@ -125,7 +125,7 @@ def generate_plot(
 
     # only add label to bottom plots
     for ax in axes[1]:
-        ax.set_xlabel("z/h")
+        ax.set_xlabel("$z/z_s$")
 
     axes.shape = axes.size
     for i, settings in enumerate(param_names):
